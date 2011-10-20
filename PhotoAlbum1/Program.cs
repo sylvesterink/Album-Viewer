d2cf7a9b73fc293d4dad3ed8d587e3cbd3f95a50
David Carver <carve006@csusm.edu>
2011-10-20 13:40:45 -0700

    add single instance of program check

--- PhotoAlbum1/Program.cs
+++ PhotoAlbum1/Program.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace PhotoAlbumViewOfTheGods
 {
     static class Program
     {
+        public static Process PriorProcess()
+        // URL: http://www.ai.uga.edu/mc/SingleInstance.html
+        // Returns a System.Diagnostics.Process pointing to
+        // a pre-existing process with the same name as the
+        // current one, if any; or null if the current process
+        // is unique.
+        {
+            Process curr = Process.GetCurrentProcess();
+            Process[] procs = Process.GetProcessesByName(curr.ProcessName);
+            foreach (Process p in procs)
+            {
+                if ((p.Id != curr.Id) &&
+                    (p.MainModule.FileName == curr.MainModule.FileName))
+                    return p;
+            }
+            return null;
+        }
         /// <summary>
         /// Photo Album Application that allows the user to create albums, fill them with pictures,
         /// and modify their attributes such as: name and description.
@@ -21,6 +39,11 @@ namespace PhotoAlbumViewOfTheGods
         [STAThread]
         static void Main()
         {
+            if (PriorProcess() != null)
+            {
+                MessageBox.Show("You can only run one instance of PhotoAlbumViewOfTheGods at a time.","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form_Main());
